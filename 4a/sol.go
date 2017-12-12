66e9133cc02c80ed1fcb1f3141238160499c91cd
Nathan Kidd <nathan@spicycrypto.ca>
2017-12-12 14:39:35 -0500

    Solution for 4a

---
+++ 4a/sol.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+import "os"
+import "bufio"
+import "strings"
+
+func main() {
+	file, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	numvalid := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		words := make(map[string]int)
+		valid := true
+		for _, token := range strings.Fields(scanner.Text()) {
+			words[token]++
+			if words[token] > 1 {
+				valid = false
+			}
+		}
+		if valid {
+			numvalid++
+		}
+	}
+
+	fmt.Printf("Num valid: %d\n", numvalid)
+}
