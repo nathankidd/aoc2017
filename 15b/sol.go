64e218580ef0fe827f5f6d885bb233ba22c9443a
Nathan Kidd <nathan@spicycrypto.ca>
2018-01-16 21:47:23 -0500

    A more sensible approach for 15b

--- 15b/sol.go
+++ 15b/sol.go
@@ -1,48 +1,35 @@
 package main
 
 import "fmt"
+import "math/bits"
 
-func newGenerator(init, factor uint64) func() uint64 {
-	n := uint64(init)
+func newGenerator(init, factor uint64, multiple uint) func() uint64 {
+	n := init
+	if bits.OnesCount(multiple) != 1 {
+		panic("multiple is not a power of two")
+	}
+	mask := uint64(multiple - 1)
 	return func() uint64 {
-		n *= factor
-		n %= 2147483647 // TODO &= 0x7fffffff something
+		for {
+			n *= factor
+			n %= 2147483647 // TODO &= 0x7fffffff something
+			if n & mask == 0 {
+				break
+			}
+		}
 		return n
 	}
 }
 
 func main() {
-	// Example inputs
-	//gena := newGenerator(65, 16807)
-	//genb := newGenerator(8921, 48271)
-
-	// Real inputs
-	gena := newGenerator(618, 16807)
-	genb := newGenerator(814, 48271)
+	gena := newGenerator(618, 16807, 4)
+	genb := newGenerator(814, 48271, 8)
 
 	pairs := 0
 
 	for i := 0; i < 5*1000*1000; i++ {
-		var a uint64
-		var b uint64
-		for {
-			a = gena()
-			if a & 3 == 0 {
-				break
-			}
-		}
-		for {
-			b = genb()
-			if b & 7 == 0 {
-				break
-			}
-		}
-		if false && i < 5 {
-			fmt.Printf("A=%d\t", a)
-			fmt.Printf("B=%d\n", b)
-		}
-		a &= 0xFFFF
-		b &= 0xFFFF
+		a := gena() & 0xffff
+		b := genb() & 0xffff
 		if a == b {
 			pairs++
 		}
