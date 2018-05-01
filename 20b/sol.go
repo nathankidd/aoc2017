999b82ed1de13a2cde4b4b1d5a2e930c261892ee
Nathan Kidd <nathan@spicycrypto.ca>
2018-04-30 22:31:28 -0400

    20b stop as soon as additional collisions are not possible
    We determine no more collisions can occur by checking the particle
    acceleration vs velocity vs position sort order.
    
    This works, but no time was spent optimizing it.

--- 20b/sol.go
+++ 20b/sol.go
@@ -3,13 +3,7 @@ package main
 import "fmt"
 import "os"
 import "bufio"
-
-func Abs(v int) int {
-	if v >= 0 {
-		return v
-	}
-	return -v
-}
+import "sort"
 
 type Point struct {
 	x int
@@ -23,7 +17,55 @@ type Particle struct {
 	acc Point
 }
 
-const MaxInt = int(^uint(0) >> 1)
+type Pair struct {
+	order int
+	val   int
+}
+
+func loose_order_match(n int, a []Pair, b []Pair) bool {
+	if a[n].order == b[n].order {
+		return true
+	}
+	// Having the same value in an adjacent position is close enough
+	// Check lower array positions
+	for i := n; i >= 0 && b[n].val == b[i].val; i-- {
+		if a[n].order == b[i].order {
+			return true
+		}
+	}
+	// And later array positions
+	for i := n; i < len(b) && b[n].val == b[i].val; i++ {
+		if a[n].order == b[i].order {
+			return true
+		}
+	}
+	return false
+}
+
+// Check for order
+// Once the particle with greatest or equal acceleration also
+// has greatest or equal velocity and greatest or equal position
+// for a given plane it is no longer possible to have a collision.
+// X plane picked here, any can work
+func particles_sorted(field map[int]Particle) bool {
+	var pos []Pair
+	var vel []Pair
+	var acc []Pair
+	for num, p := range field {
+		pos = append(pos, Pair{num, p.pos.x})
+		vel = append(vel, Pair{num, p.vel.x})
+		acc = append(acc, Pair{num, p.acc.x})
+	}
+	sort.Slice(pos[:], func(i, j int) bool { return pos[i].val < pos[j].val })
+	sort.Slice(vel[:], func(i, j int) bool { return vel[i].val < vel[j].val })
+	sort.Slice(acc[:], func(i, j int) bool { return acc[i].val < acc[j].val })
+	for n := 0; n < len(pos); n++ {
+		if !loose_order_match(n, pos, vel) || !loose_order_match(n, pos, acc) {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
 	field := make(map[int]Particle)
@@ -45,8 +87,7 @@ func main() {
 		num++
 	}
 
-	// TODO determine when no more particles can collide
-	for i := 0; i < 1000; i++ {
+	for i := 0; true; i++ {
 		collisions := make(map[Point][]int)
 
 		// Update positions
@@ -70,10 +111,16 @@ func main() {
 				for _, n := range cols {
 					delete(field, n)
 				}
-
 			}
 		}
+
+		// Check if no more collisions can occur
+		if particles_sorted(field) {
+			fmt.Printf("Order stabilized at iteration %d\n", i)
+			break
+		}
+
 	}
 
-	fmt.Printf("Left: %d\n", len(field))
+	fmt.Printf("Particles remaining: %d\n", len(field))
 }
